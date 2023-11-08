24d1c289fd4f78d87fd0b2786d4d6edbd6c0e253
Rahul Yadav <ryrahul12345@gmail.com>
2023-11-08 22:16:25 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constant";
+// require ('dotenv').config({path: './.env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./env" 
+    
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT||8000, () => {
+            console.log(`Server is listening on port ${process.env.PORT}`);
+        } );
+})
+    .catch((error) => {
+        console.log("MONGODB connect fail !!! ", error);
+        process.exit(1);
+    });
+
+
+
+
+
+
+
+
+
 
 
 
